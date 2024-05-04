88d3378a8c04dfa357186e450998788ee0b3605c
ggwpmail2 <ggwpmail2@gmail.com>
2024-05-05 02:44:39 +0800

    same

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const jsToBottomNoModule = () => {
+  return {
+    name: "no-attribute",
+    transformIndexHtml(html) {
+      html = html.replace(`type="module" crossorigin`, "")
+      let scriptTag = html.match(/<script[^>]*>(.*?)<\/script[^>]*>/)[0]
+      console.log("\n SCRIPT TAG", scriptTag, "\n")
+      html = html.replace(scriptTag, "")
+      html = html.replace("<!-- # INSERT SCRIPT HERE -->", scriptTag)
+      return html;
+    }
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    jsToBottomNoModule(),
   ],
   resolve: {
     alias: {
